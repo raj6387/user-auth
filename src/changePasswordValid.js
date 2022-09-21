192476fc89301ae4f73fcd22c729dc9a6fc0eee3
Raj Singh <rajsinghcuilsoft@gmail.com>
2022-09-21 11:39:43 +0530

    Format code.

--- src/changePasswordValid.js
+++ src/changePasswordValid.js
@@ -1,8 +1,7 @@
-const joi=require('joi');
-const schema=joi.object({
-    oldPassword:joi.string().required().messages(),
-    newPassword:joi.string().required().messages(),
-    confirmPassword:joi.string().required().messages()
-
-})
-module.exports={schema};
+const joi = require("joi");
+const schema = joi.object({
+  oldPassword: joi.string().required().messages(),
+  newPassword: joi.string().required().messages(),
+  confirmPassword: joi.string().required().messages(),
+});
+module.exports = { schema };
